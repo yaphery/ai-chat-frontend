cc4d226aacb2302f6f03b6d4d759d199e87d7347
yaphery <yyue437@gmail.com>
2026-03-21 17:32:20 +0800

    publicPath

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
-const ChatPage = () => import('../pages/chat-index.vue')
-const CheckIndex = () => import('../pages/check-index.vue')
+const ChatPage = () => import("../pages/chat-index.vue");
+const CheckIndex = () => import("../pages/check-index.vue");
 
 const routes = [
-  { path: '/', name: 'Chat', component: ChatPage },
-  { path: '/check', name: 'CheckIndex', component: CheckIndex }
-]
+  { path: "/", name: "Chat", component: ChatPage },
+  { path: "/check", name: "CheckIndex", component: CheckIndex },
+];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
-})
+  history: createWebHistory("/ai-chat-frontend/"),
+  routes,
+});
 
-export default router
+export default router;
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
+  publicPath: '/ai-chat-frontend/',
   transpileDependencies: true,
   lintOnSave: false,
   configureWebpack: {
@@ -9,4 +10,3 @@ module.exports = defineConfig({
     }
   }
 })
-
